3f173ef1edd5ac62f9253a036926514449e001f0
thanathon <rung_fung@hotmail.co.th>
2026-04-16 01:13:55 +0700

    Initial commit

---
+++ app/about/error.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+const error = ({error}) => {
+    console.log(error)
+  return (
+    <div>
+        <h1>Error 101</h1>
+    </div>
+  )
+}
+export default error
---
+++ app/about/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+const url = "https://jsonplaceholder.typicode.com/todos";
+
+const fetchTodos = async () => {
+
+    await new Promise((resolve)=>setInterval(resolve, 1000))
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  return data;
+};
+
+const AboutPage = async () => {
+  const data = await fetchTodos();
+  console.log(data);
+
+  return (
+    <div>
+        {
+            data.map((item, index) => {
+                return <li key={index}>{item.title}</li>
+            })
+        }
+    </div>
+  );
+};
+export default AboutPage;
